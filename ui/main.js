d1daf63ec217b4b1d1045ee8bf6b3a25ab134b73
Bhagyashree-Rawal <bhagyashreerawal17@gmail.com>
2016-09-29 17:55:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,9 @@
 console.log('Loaded!');
+//change the main-text
+var element=document.getElementByID('main-text');
+element.innerHTML='Hi I am Bhagyashree Rawal. \n This is my first web app.';
+//move the image
+var img=document.getElementByID('madi');
+img.onclick=function() {
+    img.style.marginLeft= '100px';
+};
